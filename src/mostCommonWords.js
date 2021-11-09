034e06599a63ec1e406f99a4b0d6c639f54f0677
danroche10 <danielroche10@gmail.com>
2021-11-09 09:29:17 +0000

    initial mostCommonWords solution with test passing for array of five with three different words

--- src/mostCommonWords.js
+++ src/mostCommonWords.js
@@ -2,9 +2,23 @@
 function mostCommonWords(array) {
   if (array.length <= 2) return array;
   else {
-    return array.filter((element, index, array) => {
-      return array.indexOf(element) === index;
+    let counts = {};
+    for (const word of array) {
+      counts[word] = counts[word] ? counts[word] + 1 : 1;
+    }
+    sortedCounts = [];
+    for (word in counts) {
+      sortedCounts.push([word, counts[word]]);
+    }
+    sortedCounts.sort(function (a, b) {
+      if (b[1] > a[1]) {
+        return 1;
+      }
+      if (b[1] < a[1]) {
+        return -1;
+      }
     });
+    return [sortedCounts[0][0], sortedCounts[1][0]];
   }
 }
 
